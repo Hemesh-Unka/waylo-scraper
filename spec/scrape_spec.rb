7a486a9e182fed0edb7200cd1403925dd7a5a9a6
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-05 20:29:04 +0100

    refactoring scrape class tests

--- spec/scrape_spec.rb
+++ spec/scrape_spec.rb
@@ -1,15 +1,9 @@
 require 'scrape'
-require 'open-uri'
 
 describe Scrape do
   describe '.url' do
-    it 'takes a url' do
+    it 'takes a url and returns a nokogiri object' do
       expect(Scrape).to receive(:open).with('http://www.test.com')
-
-      Scrape.url('http://www.test.com')
-    end
-
-    it 'nokogiri gets called and returns a nokogirl object' do
       expect(Nokogiri::HTML::Document).to receive(:parse)
 
       Scrape.url('http://www.test.com')
